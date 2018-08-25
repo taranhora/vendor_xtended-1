c990b3b402968387a5ab3fa6899ed8b66c0e9821
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    vendor: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigXtended.mk
+++ config/BoardConfigXtended.mk
@@ -3,6 +3,10 @@ ifeq ($(WITH_LINEAGE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/xtended/config/BoardConfigQcom.mk
+endif
+
 include vendor/xtended/config/BoardConfigKernel.mk
 
 include vendor/xtended/config/BoardConfigSoong.mk
