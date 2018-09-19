81562735e6e25ec86f50c13888eee1e6d864da79
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    arrow: Dynamically generate kernel headers using arrow generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: I6891638358a60f2821c456b2a2a148c60eb35ca0

--- config/BoardConfigXtended.mk
+++ config/BoardConfigXtended.mk
@@ -3,4 +3,6 @@ ifeq ($(WITH_LINEAGE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
 
-include vendor/arrow/config/BoardConfigKernel.mk
+include vendor/xtended/config/BoardConfigKernel.mk
+
+include vendor/xtended/config/BoardConfigSoong.mk
